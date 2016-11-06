f103626d617c6438b1eef8c9d50ded35980c37b3
Gabriele Cipriano <gabri.cipriano@gmail.com>
2016-11-06 10:41:07 +0100

    return FizzBuzz accordingly to the rules

--- functions2/test/functions2_test.exs
+++ functions2/test/functions2_test.exs
@@ -2,6 +2,7 @@ defmodule Functions2Test do
   use ExUnit.Case
   doctest Functions2
 
+  def fizzBuzz(0, 0, _), do: "FizzBuzz."
   def fizzBuzz(0, _, _), do: "Fizz."
   def fizzBuzz(_, 0, _), do: "Buzz."
 
@@ -9,7 +10,11 @@ defmodule Functions2Test do
     assert fizzBuzz(0, 34, 25) == "Fizz."
   end
 
-  test "FizzBuzz" do
+  test "Buzz" do
     assert fizzBuzz(21, 0, 145) == "Buzz."
+  end
+
+  test "FizzBuzz" do
+    assert fizzBuzz(0, 0, 145) == "FizzBuzz."
   end
 end
