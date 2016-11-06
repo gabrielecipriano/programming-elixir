02f274e383aa2e4a8063f78e2443b6b6599fc396
Gabriele Cipriano <gabri.cipriano@gmail.com>
2016-11-06 11:10:50 +0100

    functions4 implemented

--- functions4/test/functions4_test.exs
+++ functions4/test/functions4_test.exs
@@ -2,7 +2,12 @@ defmodule Functions4Test do
   use ExUnit.Case
   doctest Functions4
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  def prefix(prefix) do
+      (fn(suffix) -> "#{prefix} #{suffix}" end)
+  end
+
+  test "prefix" do
+    assert prefix("Mrs").("Smith") == "Mrs Smith"
+    assert prefix("Elixir").("Rocks") == "Elixir Rocks"
   end
 end
